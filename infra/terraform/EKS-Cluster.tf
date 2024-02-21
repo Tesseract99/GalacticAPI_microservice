f5fbb3a635858d3b1d3eda1779dcc554e7bfa474
Prithvi <prithvirajp2899@gmail.com>
2024-02-21 22:13:49 +0530

    testing prometheus and graphana integration

--- infra/terraform/EKS-Cluster.tf
+++ infra/terraform/EKS-Cluster.tf
@@ -20,7 +20,7 @@ resource "aws_eks_node_group" "tour-app-nodegrp" {
   subnet_ids      = [var.subnet-tourapp-nodegrp]
 
   scaling_config {
-    desired_size = 1
+    desired_size = 2
     max_size     = 2
     min_size     = 1
   }
