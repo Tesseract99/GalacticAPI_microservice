16ab450e3770deffa359213845c65b277ce2fb6e
Prithvi <prithvirajp2899@gmail.com>
2024-02-25 16:50:52 +0530

    fixed prom client incorrect instantiation of counter

--- view/controller/prometheusController.js
+++ view/controller/prometheusController.js
@@ -1,10 +1,7 @@
 const promClient = require("prom-client");
 
-// console.log(promClient);
-const counter = new promClient.Counter();
 const register = new promClient.Registry(); // Instantiate a registry
-
-const requestCounter = counter({
+const requestCounter = new promClient.Counter({
   name: "my_requests_total",
   help: "Total number of requests.",
   registers: [register], // Register the counter
