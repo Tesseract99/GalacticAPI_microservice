86b7af057afefaa03a42fa6297832aa68e762e37
Prithvi <prithvirajp2899@gmail.com>
2024-02-25 18:09:03 +0530

    Fixed syntax

--- view/routes/prometheusRoutes.js
+++ view/routes/prometheusRoutes.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const router = express.Router();
 const { register } = require("../controller/prometheusController");
-console.log(await register.metrics());
 
 router.get("/", async (req, res) => {
   try {
