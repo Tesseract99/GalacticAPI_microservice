719a90f46defd680fd44a5215bbc2c7e6942283a
Prithvi <prithvirajp2899@gmail.com>
2024-02-25 17:06:54 +0530

    prometheus fixed import issue

--- view/routes/prometheusRoutes.js
+++ view/routes/prometheusRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const register = require("../controller/prometheusController");
+const { register } = require("../controller/prometheusController");
 
 router.get("/", (req, res) => {
   console.log(register.metrics());
